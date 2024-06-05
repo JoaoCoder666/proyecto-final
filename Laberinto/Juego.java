007c52368983531a05fba21c24f6f9175955e961
Juan Galindo <juqnestgm05290@gmail.com>
2024-06-05 16:20:31 -0500

    Version 1.0

---
+++ Laberinto/Juego.java
@@ -0,0 +1,79 @@
+package Laberinto;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Juego extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Laberinto laberinto = new Laberinto ();
+	Personaje personaje = new Personaje ();
+	
+	public Juego () {
+		addKeyListener(new KeyListener(){
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				personaje.teclaPresionada(e);
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}});
+		setFocusable(true);
+		
+	}
+	
+	public void paint (Graphics grafico) {
+		
+		laberinto.paint(grafico);
+		personaje.paint(grafico);
+		
+		
+	}
+
+	public static void main (String []args) throws InterruptedException {
+		
+		JFrame miVentana = new JFrame ("Laberinto");
+		
+		Juego game = new Juego ();
+		
+		miVentana.add(game); 
+		miVentana.setSize(920, 540);
+		miVentana.setLocation(300, 200);
+		miVentana.setVisible(true);
+		
+		miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		while(true) {
+			
+			Thread.sleep(10);
+			game.repaint();
+			
+			
+		}
+		
+		
+		
+	}
+	
+
+}
---
+++ Laberinto/Personaje.java
@@ -0,0 +1,51 @@
+package Laberinto;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class Personaje {
+	Laberinto lab = new Laberinto();
+	private int x = 40, y = 40, ancho = 40, alto = 40, movimiento = 40;	
+	
+	public void paint (Graphics grafico) {
+		
+		grafico.setColor(Color.red);
+		grafico.fillOval(x, y, ancho, alto);
+		
+		grafico.setColor(Color.black);
+		grafico.drawOval(x, y, ancho, alto);
+		
+	}
+	
+	public void teclaPresionada (KeyEvent evento) {
+		int [][] laberinto = lab.obtieneLaberinto();
+		
+		//Hacia la izquierda
+		if(evento.getKeyCode() == 37) {//Hacia la izquierda
+			if(laberinto [y /40][(x/40)-1] != 1) {
+				x = x - movimiento;
+			}
+
+		}if(evento.getKeyCode() == 39) {//Hacia la derecha
+			if(laberinto [y /40][(x/40)+1] != 1) {
+				x = x + movimiento;
+			}
+		
+			
+		}if(evento.getKeyCode() == 40) {//Hacia abajo
+			if(laberinto [(y /40)+1][(x/40)] != 1) {
+				y = y + movimiento;
+			}
+			
+			
+		}if(evento.getKeyCode() == 38) {//Hacia arriba
+			if(laberinto [(y /40)-1][(x/40)] != 1) {
+				y = y - movimiento;
+			}
+			
+		}
+		
+	}
+
+}
